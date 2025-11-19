7f9bdf23fc21992ef1e15bb193c59e3cd315ee99
DanielMaku-STUDENT <G00467390@atu.ie>
2025-11-19 17:39:13 +0000

    finished step 1

---
+++ src/test/java/ie/atu/BankAccountTest.java
@@ -0,0 +1,47 @@
+package ie.atu;
+
+import ie.atu.week9.BankAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class BankAccountTest {
+
+    BankAccount account;
+
+    @BeforeEach
+    void setUp()
+    {
+        account = new BankAccount();
+    }
+
+    @Test
+    void constructorInitialisation()
+    {
+        account = new BankAccount("ACC12345", "Daniel", 500);
+        assertEquals("ACC12345", account.getAccNo());
+        assertEquals("Daniel", account.getName());
+        assertEquals(500, account.getBalance());
+    }
+
+    @Test
+    void constructorNegativeInitialisation()
+    {
+        Exception ex = assertThrows(IllegalArgumentException.class,()-> new BankAccount("ACC12345", "Daniel", -1));
+        assertEquals("Balance must be greater than 0.", ex.getMessage());
+    }
+
+
+   // @Test
+  //  void deposit(20, BankAccount.deposit)
+  //  {
+
+   // }
+
+
+
+
+}
